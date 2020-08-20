e461e25cacd96f86a2e650125c1661bf77ddaa4a
Almas Baimagambetov <almaslvl@gmail.com>
2020-08-20 09:08:10 +0100

    initial setup

---
+++ src/main/java/com/almasb/gamejam/GameJam1App.java
@@ -0,0 +1,18 @@
+package com.almasb.gamejam;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+/**
+ * @author Almas Baimagambetov (almaslvl@gmail.com)
+ */
+public class GameJam1App extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
